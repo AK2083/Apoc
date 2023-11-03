9f9a4866660f82021843991218204febd5dd43d2
Adrian Helmchen <adrian.helmchen@outlook.de>
2023-11-03 14:15:59 +0100

    feat(Apoc): entry tax parameter

---
+++ Apoc/ITax.cs
@@ -0,0 +1,19 @@
+﻿using Apoc.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apoc
+{
+    public interface ITax
+    {
+        IInternalParameter InternalPara { get; set; }
+        IInputParameter InputPara { get; set; }
+        IOutputParameter OutputPara { get; set; }
+        IOutputDBA OutputDBASE { get; set; }
+
+        void Init();
+    }
+}
