9639ac59cfd746f7897b5a22842693bd4591fafb
Adrian Helmchen <adrian.helmchen@outlook.de>
2023-11-05 16:34:14 +0100

    fix(Apoc): add null tax object

---
+++ Apoc/NullTax.cs
@@ -0,0 +1,16 @@
+﻿using Apoc.Model;
+
+namespace Apoc
+{
+    public sealed class NullTax : ITax
+    {
+        private static readonly ITax nullTax = new NullTax();
+        public static ITax Instance { get { return nullTax; } }
+
+        public IInputParameter InputPara { get; set; }
+
+        public NullTax() {}
+
+        public void Init() {}
+    }
+}
